dd4bfdec950241cd53041f171ede3309f00fff41
mnasruul <muhammad.nasrul@pt-ssss.com>
2021-01-19 15:47:07 +0700

    a

--- mvc/controllers/user_controller.go
+++ mvc/controllers/user_controller.go
@@ -1,7 +1,27 @@
 package controllers
 
-import "net/http"
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"strconv"
+)
 
 func GetUser(resp http.ResponseWriter, req *http.Request) {
+	userId, err := strconv.ParseInt(req.URL.Query().Get("user_id"), 10, 64)
+	if err != nil {
+		resp.WriteHeader(http.StatusNotFound)
+		resp.Write([]byte("user_id must ne a number"))
+		return
+	}
 
+	user, err := services.GetUser(userId)
+	if err != nil {
+		resp.WriteHeader(http.StatusNotFound)
+		resp.Write([]byte(err.Error()))
+		return
+	}
+
+	jsonValue, _ := json.Marshal(user)
+	resp.Write(jsonValue)
 }
